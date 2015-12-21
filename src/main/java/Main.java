18d06476b34a7608e0c9803838e93ebadf3c3447
jchio001 <jchio001@ucr.edu>
2015-12-20 16:57:12 -0800

    Creating table with POST

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,34 +29,48 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Main extends HttpServlet{
 
-  private static String TABLE_CREATION = "";
+    private static String TABLE_CREATION = "CREATE TABLE USER (user_id SERIAL, name varchar(32), " +
+          "about_me varchar(1024), num_wrong int, run_type varchar(32), time_occurred bigint)";
 
-  private static Connection getConnection() throws URISyntaxException, SQLException
-  {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-    String username = dbUri.getUserInfo().split(":")[0];
-    String password = dbUri.getUserInfo().split(":")[1];
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-
-    return DriverManager.getConnection(dbUrl, username, password);
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      Connection connection = null;
+      try {
+        connection = getConnection();
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(TABLE_CREATION);
+      }
+      catch (Exception e) {
+        resp.setStatus(500);
+        resp.getWriter().print("Table creation error: " + e.getMessage());
+      }
   }
 
-  public static void main(String[] args) throws Exception {
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-    context.setContextPath("/");
-    server.setHandler(context);
-    context.addServlet(new ServletHolder(new Main()), "/*");
-    server.start();
-    server.join();
-  }
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+      URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-  public static String getStackTrace(Throwable aThrowable) {
-    final Writer result = new StringWriter();
-    final PrintWriter printWriter = new PrintWriter(result);
-    aThrowable.printStackTrace(printWriter);
-    return result.toString();
-  }
+      String username = dbUri.getUserInfo().split(":")[0];
+      String password = dbUri.getUserInfo().split(":")[1];
+      String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+
+      return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    public static void main(String[] args) throws Exception {
+      Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+      ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+      context.setContextPath("/");
+      server.setHandler(context);
+      context.addServlet(new ServletHolder(new Main()), "/*");
+      server.start();
+      server.join();
+    }
+
+    public static String getStackTrace(Throwable aThrowable) {
+      final Writer result = new StringWriter();
+      final PrintWriter printWriter = new PrintWriter(result);
+      aThrowable.printStackTrace(printWriter);
+      return result.toString();
+    }
 
 }
