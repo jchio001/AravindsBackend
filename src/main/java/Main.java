50cf1b48dd6c7745beb3795a38ecb24d96889609
jchio001 <jchio001@ucr.edu>
2015-12-21 18:33:37 -0800

    Something's wrong with POST.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -47,7 +47,7 @@ public class Main extends HttpServlet{
 
 	  try {
 		  JSONObject jsonObject = new JSONObject(jb.toString());
-		  if (req.getRequestURI().endsWith("/createAccount")) {
+		  if (req.getRequestURI().endsWith("/create")) {
 			  String name = jsonObject.getString("name");
 			  String about_me = jsonObject.getString("about_me");
 			  String village = jsonObject.getString("village");
