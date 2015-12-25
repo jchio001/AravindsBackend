b10358db23cb6f0cf56a61d21aaa7ab9a128e4af
Gear61 <chessnone@yahoo.com>
2015-12-24 18:57:54 -0800

    Going back to a clean slate

---
+++ src/main/java/Constants.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alexanderchiou on 12/24/15.
+ */
+public class Constants {
+    public static final int BAD_REQUEST = 400;
+    public static final int NOT_FOUND = 404;
+    public static final int INTERNAL_SERVER_ERROR = 500;
+
+    public static final String READ_BODY_FAIL = "Failed to process the request body.";
+    public static final String BAD_BODY_MESSAGE = "Request body wasn't a valid JSON.";
+    public static final String DB_CONNECTION_FAIL = "Unable to connect to database.";
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,124 +13,82 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
-public class Main extends HttpServlet{
-
-	private static String TABLE_CREATION = "CREATE TABLE IF NOT EXISTS profile (user_id SERIAL, name varchar(32), " +
-            "about_me varchar(1024), village varchar(32), zip_code int, phone_number varchar(16), email varchar(32));";
-    private static String TABLE_CREATION_2 = "CREATE TABLE IF NOT EXISTS Connections (requester_id int, target_id int, status varchar(32));";
-
+public class Main extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Connection connection = null;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 		try {
-			connection = getConnection();
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate(TABLE_CREATION);
-			stmt.executeUpdate(TABLE_CREATION_2);
-			resp.getWriter().print("I did get X.");
-		}
-		catch (Exception e) {
-			resp.setStatus(500);
-			resp.getWriter().print("Table creation error: " + e.getMessage());
-		}
+			Connection connection = getConnection(response);
+			if (connection != null) {
+				String path = request.getRequestURI();
+				String[] pathPieces = path.split("/");
 
-		finally {
-			try {
+				response.getWriter().print("GET!");
+
 				connection.close();
 			}
-			catch (SQLException e) {
-				resp.getWriter().print("Failed to close connection: " + getStackTrace(e));
-			}
+		}
+		catch (Exception e) {
+			response.setStatus(Constants.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException
-	{
-		Connection connection = null;
-		try {
-			connection = getConnection();
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate(TABLE_CREATION);
-			stmt.executeUpdate(TABLE_CREATION_2);
-			resp.getWriter().print("I did post.");
-		}
-		catch (Exception e) {
-			resp.setStatus(500);
-			resp.getWriter().print("Table creation error: " + e.getMessage());
-		}
-
-		StringBuffer jb = new StringBuffer();
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Get request body into string
+		StringBuilder requestBody = new StringBuilder();
 		String line;
 		try {
-			BufferedReader reader = req.getReader();
-			while ((line = reader.readLine()) != null)
-				jb.append(line);
-		}
-		catch (IOException e) {
-			resp.setStatus(400);
-			resp.getWriter().print("Couldn't read in request body: " + getStackTrace(e));
-		}
-
-		try {
-			JSONObject jsonObject = new JSONObject(jb.toString());
-			if (req.getRequestURI().endsWith("/createAccount/")) {
-				resp.setStatus(200);
-				resp.getWriter().print("Creating!");
-				String name = jsonObject.getString("name");
-				String about_me = jsonObject.getString("about_me");
-				String village = jsonObject.getString("village");
-				String zip_code = jsonObject.getString("zip_code");
-				String phone_number = jsonObject.getString("phone_number");
-				String email = jsonObject.getString("email");
-				String update_sql = "INSERT INTO profile (name, about_me, village, zip_code, phone_number, email) VALUES (?, ?, ?, ?, ?, ?)";
-				try {
-					PreparedStatement stmt = connection.prepareStatement(update_sql);
-					stmt.setString(1, name);
-					stmt.setString(2, about_me);
-					stmt.setString(3, village);
-					stmt.setString(4, zip_code);
-					stmt.setString(5, phone_number);
-					stmt.setString(6, email);
-					stmt.executeUpdate();
-					stmt.close();
-				}
-				catch (SQLException e) {
-					resp.getWriter().print("SQL ERROR @POST: " + getStackTrace(e));
-				}
-			}
-			else {
-				resp.setStatus(404);
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null) {
+				requestBody.append(line);
 			}
 		}
-		catch (JSONException e1) {
-			resp.setStatus(400);
-			resp.getWriter().print("Error parsing request JSON: " + getStackTrace(e1));
-		}
 		catch (IOException e) {
-			resp.setStatus(500);
-			resp.getWriter().print("Error creating SQL statement: " + getStackTrace(e));
+			response.setStatus(Constants.INTERNAL_SERVER_ERROR);
+			response.getWriter().print(Constants.READ_BODY_FAIL);
+			return;
 		}
 
-		finally {
+		// Get connection to DB
+		Connection connection = getConnection(response);
+
+		if (connection != null) {
+			// Business logic
 			try {
-				connection.close();
+				JSONObject jsonObject = new JSONObject(requestBody.toString());
+				String path = request.getRequestURI();
+				String[] pathPieces = path.split("/");
+
+                response.getWriter().print("POST!");
+            }
+			catch (JSONException e) {
+				response.setStatus(Constants.BAD_REQUEST);
+				response.getWriter().print(Constants.BAD_BODY_MESSAGE);
 			}
-			catch (SQLException e) {
-				resp.getWriter().print("Failed to close connection: " + getStackTrace(e));
+			finally {
+				try {
+					connection.close();
+				}
+				catch (SQLException ignored) {}
 			}
 		}
 	}
 
-	private static Connection getConnection() throws URISyntaxException, SQLException {
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-		String username = dbUri.getUserInfo().split(":")[0];
-		String password = dbUri.getUserInfo().split(":")[1];
-		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-
-		return DriverManager.getConnection(dbUrl, username, password);
+	private static Connection getConnection(HttpServletResponse response) throws IOException {
+		try {
+			URI dbUri = new URI(System.getenv("DATABASE_URL"));
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+			return DriverManager.getConnection(dbUrl, username, password);
+		}
+		catch (URISyntaxException|SQLException e) {
+			response.setStatus(Constants.INTERNAL_SERVER_ERROR);
+			response.getWriter().print(Constants.DB_CONNECTION_FAIL);
+			return null;
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -142,12 +100,4 @@ public class Main extends HttpServlet{
 		server.start();
 		server.join();
 	}
-
-	public static String getStackTrace(Throwable aThrowable) {
-      final Writer result = new StringWriter();
-      final PrintWriter printWriter = new PrintWriter(result);
-      aThrowable.printStackTrace(printWriter);
-      return result.toString();
-    }
-
 }
