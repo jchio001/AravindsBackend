696461fce2217f3d3368d218b4a39f8b5631dff1
jchio001 <jchio001@ucr.edu>
2015-12-28 15:30:34 -0800

    added changeVisibility function.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,6 +34,8 @@ public class Main extends HttpServlet {
 					getSuggestions.getSuggestions(request, response, connection, pathPieces[2]);
 				else if (pathPieces[1].equals("pendingconnections") && pathPieces.length == 3)
 					getPendingConnCnt.getPendingConnCnt(request, response, connection, pathPieces[2]);
+				else if (pathPieces[1].equals("changevisibility") && pathPieces.length == 4)
+					changeVisibility.changeVisible(request, response, connection, pathPieces[2], pathPieces[3]);
 				else if (pathPieces[1].equals("connections")) {
 					if ((pathPieces[2].equals("sent") || pathPieces[2].equals("received")) && pathPieces.length == 4)
 						sentOrRcvdConn.getSentOrRcvdConns(request, response, connection, pathPieces[2], pathPieces[3]);
---
+++ src/main/java/changeVisibility.java
@@ -0,0 +1,51 @@
+import Misc.Constants;
+import org.json.JSONException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by jman0_000 on 12/28/2015.
+ */
+public class changeVisibility {
+	public static void changeVisible(HttpServletRequest req, HttpServletResponse resp, Connection connection, String mode, String id)
+	throws IOException {
+		try {
+			long src_id;
+			try {
+				src_id = Long.parseLong(id);
+			}
+			catch (NumberFormatException e) {
+				resp.setStatus(Constants.BAD_REQUEST);
+				resp.getWriter().print(e.getMessage());
+				return;
+			}
+
+			String update_sql = "UPDATE Profile SET visibility = ? where user_id = ?";
+			PreparedStatement stmt = connection.prepareStatement(update_sql);
+
+			if (mode.equals("true"))
+				stmt.setBoolean(1, true);
+			else if (mode.equals("false"))
+				stmt.setBoolean(1, false);
+			else {
+				resp.setStatus(Constants.BAD_REQUEST);
+				return;
+			}
+
+			stmt.setLong(2, src_id);
+			stmt.executeUpdate();
+		}
+		catch (SQLException exception) {
+			resp.setStatus(Constants.INTERNAL_SERVER_ERROR);
+			resp.getWriter().print(Main.getStackTrace(exception));
+		}
+
+	}
+
+}
